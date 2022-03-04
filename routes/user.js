0ef3bae02d9d1dcfdd676d8e4e3ca053a7568e38
YukiXueyan <1070152568@qq.com>
2022-03-04 20:25:52 +0800

    add mysql

--- routes/user.js
+++ routes/user.js
@@ -19,6 +19,8 @@ route.get('/select', function(req, res) {
     con.query(sql, function(err, result) { //连接数据库 传入sql语句 回调函数得到结果
         try {
             res.send(result) //查询结果响应给请求方
+            console.log("查询success");
+
         } catch (err) {
             console.log("查询失败");
         }
@@ -26,13 +28,37 @@ route.get('/select', function(req, res) {
 })
 //增加数据
 route.post('/add', function(req, res) {
-    var sql = 'insert into user set  uname=? , uage=?'; // 这边的"?"是SQL的模板语法
-    var params = [req.query.uname, req.query.uage]  // 这边的数组参数与上边的"?"一一映射
+    var sql = 'insert into user set  name=? , state=?'; // 这边的"?"是SQL的模板语法
+    var params = [req.query.name, req.query.state]  // 这边的数组参数与上边的"?"一一映射
     con.query(sql, params, function(err, result) {
         try {
             res.send('增加数据成功');
         } catch (err) {
             console.log('新增数据失败');
+        }
+    });
+})
+//修改数据
+route.put('/update', function(req, res) {
+    var sql = 'update user set name=?, state=? where id=?';
+    var params = [req.query.name, req.query.state, req.query.id]
+    con.query(sql, params, function(err) {
+        try {
+            res.send('修改数据成功');
+        } catch (err) {
+            console.log('修改数据失败');
+        }
+    });
+})
+//删除数据
+route.delete('/delete', function(req, res) {
+    var sql = 'delete from user where id= ?'
+    var params = [req.query.id];
+    con.query(sql, params, function(err) {
+        try {
+            res.send('删除数据成功');
+        } catch (err) {
+            console.log('删除数据失败');
         }
     });
 })
